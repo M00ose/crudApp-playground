57f15719a207d496dababfbf3a4419e8e456aaa1
Admin <user@Users-MacBook-Pro.local>
2023-02-19 00:55:55 +0800

    Updated prospect form to match updated prospect schema

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography, FormControl, FormHelperText, TextField, TextareaAutosize, Stack, Select, MenuItem, Button} from '@pankod/refine-mui';
+import { Box, Typography, FormControl, FormHelperText, TextField, Stack, Select, MenuItem, Button} from '@pankod/refine-mui';
 
 import { FormProps } from 'interfaces/common';
 import CustomButton from './CustomButton';
+import DateTimePicker from './DateTimePicker';
 
 const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, onFinishHandler, prospectImage }: FormProps ) => {
   return (
@@ -10,7 +11,7 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
         {type} a Prospect
       </Typography>
 
-      <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+      <Box mt={2.5} borderRadius="20px" padding="20px" bgcolor="#fcfcfc">
         <form 
           style={{
             marginTop: '20px', 
@@ -20,31 +21,89 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
             gap: '20px'}}
           onSubmit={handleSubmit (onFinishHandler)}
         >
-          <FormControl>
-            <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter prospect name</FormHelperText>
-            <TextField 
-              fullWidth
-              required
-              id="outlined-basic"
-              color="info"
-              variant="outlined"
-              {...register('title', { required: true })}/>
-          </FormControl>
+          <Stack direction="row" gap={4}>
+
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Prospect Name</FormHelperText>
+              <TextField 
+                fullWidth
+                required
+                id="standard-basic"
+                color="info"
+                type="number"
+                variant="standard"
+                size="small"
+                {...register('title', { required: true })}
+              />
+            </FormControl>
+
+            <FormControl sx={{ minWidth: '150px'}} size="small">
+              <FormHelperText
+                sx={{
+                  fontFamily: 'Oswald',
+                  fontWeight: 500,
+                  margin: '10px 0',
+                  fontSize: 14,
+                  color: "#008E87"
+                }}>
+                  Select Stage
+              </FormHelperText>
+              <Select
+                variant="standard"
+                color="info"
+                inputProps={{ 'aria-label' : 'Without label' }}
+                {...register('stage', { required: true})}
+              >
+                <MenuItem value="Not Started">Not Started</MenuItem>
+                <MenuItem value="Cultivating">Cultivating</MenuItem>
+                <MenuItem value="Active">Active</MenuItem>
+                <MenuItem value="Soliciting">Soliciting</MenuItem>
+                <MenuItem value="Inactive">Inactive</MenuItem>
+
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ minWidth: '150px'}} size="small">
+              <FormHelperText
+                sx={{
+                  fontFamily: 'Oswald',
+                  fontWeight: 500,
+                  margin: '10px 0',
+                  fontSize: 14,
+                  color: "#008E87"
+                }}>
+                  Select Priority
+              </FormHelperText>
+              <Select
+                variant="standard"
+                color="info"
+                inputProps={{ 'aria-label' : 'Without label' }}
+                {...register('priority', { required: true })}
+              >
+                <MenuItem value="High">High</MenuItem>
+                <MenuItem value="Medium">Medium</MenuItem>
+                <MenuItem value="Low">Low</MenuItem>
+              </Select>
+            </FormControl>
+
+          </Stack>
+          
 
           <FormControl>
-            <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter notes</FormHelperText>
-            <TextareaAutosize
-              minRows={5}
-              required
+            <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter summary</FormHelperText>
+            <TextField
+              id="standard-textarea"
+              multiline
+              variant="standard"
               placeholder="Write a high level description of the prospect and next steps."
               color="info"
-              style={{ fontFamily: 'Source Sans Pro', width: '100%', background: 'transparent', fontSize: "16px", borderColor: 'rgba(0,0,0,0.23)', borderRadius: 6, padding: 10, color: '#919191' }}
-              {...register('note', { required: true })}
+              style={{ fontFamily: 'Source Sans Pro', width: '100%', background: 'transparent', fontSize: "16px", padding: 10, color: '#919191', borderColor: 'transparent'}}
+              {...register('summary')}
             />
           </FormControl>
 
-          <Stack direction="row" gap={4}>
-            <FormControl sx={{ flex: 1 }}>
+          <Stack>
+            <FormControl>
               <FormHelperText
                 sx={{
                   fontFamily: 'Oswald',
@@ -56,7 +115,7 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
                   Select Program
               </FormHelperText>
               <Select
-                variant="outlined"
+                variant="standard"
                 color="info"
                 displayEmpty
                 required
@@ -72,18 +131,110 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
 
               </Select>
             </FormControl>
-            <FormControl>
-            <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter potential grant amount</FormHelperText>
-            <TextField 
-              fullWidth
-              required
-              id="outlined-basic"
-              color="info"
-              type="number"
-              variant="outlined"
-              {...register('grantAmount', { required: true })}
-            />
-          </FormControl>
+          </Stack>
+
+          <Stack direction="row" gap={4}>
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter potential grant amount</FormHelperText>
+              <TextField 
+                fullWidth
+                required
+                id="standard-basic"
+                color="info"
+                type="number"
+                variant="standard"
+                {...register('grantAmount')}
+              />
+            </FormControl>
+
+            <FormControl sx={{ minWidth: '150px'}}>
+              <FormHelperText
+                sx={{
+                  fontFamily: 'Oswald',
+                  fontWeight: 500,
+                  margin: '10px 0',
+                  fontSize: 14,
+                  color: "#008E87"
+                }}>
+                  Select Grant Type
+              </FormHelperText>
+              <Select
+                variant="standard"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ 'aria-label' : 'Without label' }}
+                {...register('grantType')}
+              >
+                <MenuItem value="Unrestricted">Unrestricted</MenuItem>
+                <MenuItem value="Program">Program</MenuItem>
+                <MenuItem value="Project">Project</MenuItem>
+
+              </Select>
+            </FormControl>
+
+            <FormControl sx={{ minWidth: '150px'}}>
+              <FormHelperText
+                sx={{
+                  fontFamily: 'Oswald',
+                  fontWeight: 500,
+                  margin: '10px 0',
+                  fontSize: 14,
+                  color: "#008E87"
+                }}>
+                  Select Funding Cycle
+              </FormHelperText>
+              <Select
+                variant="standard"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ 'aria-label' : 'Without label' }}
+                defaultValue='None'
+                {...register('fundingCycle')}
+              >
+                <MenuItem value="One-time">One-time</MenuItem>
+                <MenuItem value="Annual">Annual</MenuItem>
+                <MenuItem value="Multi-year">Multi-year</MenuItem>
+
+              </Select>
+            </FormControl>
+          </Stack>
+
+          <Stack direction="row" gap={4}>
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText
+                sx={{
+                  fontFamily: 'Oswald',
+                  fontWeight: 500,
+                  margin: '10px 0',
+                  fontSize: 14,
+                  color: "#008E87"
+                }}>
+                  Select Application Process
+              </FormHelperText>
+              <Select
+                variant="standard"
+                color="info"
+                displayEmpty
+                required
+                inputProps={{ 'aria-label' : 'Without label' }}
+                {...register('applicationProcess')}
+              >
+                <MenuItem value="Open">Open</MenuItem>
+                <MenuItem value="Call">Call</MenuItem>
+                <MenuItem value="Invitation">Invitation</MenuItem>
+
+              </Select>
+            </FormControl>
+
+            <FormControl
+              inputProps={{ 'aria-label' : 'Without label' }}
+              {...register('deadline')}
+            >
+                <DateTimePicker />
+            </FormControl >
+
           </Stack>
 
           <FormControl>
@@ -91,12 +242,38 @@ const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, on
             <TextField 
               fullWidth
               required
-              id="outlined-basic"
+              id="standard-basic"
               color="info"
-              variant="outlined"
+              variant="standard"
               {...register('location', { required: true })}
             />
           </FormControl>
+          
+          <Stack direction="row" gap={4}>
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter a website url</FormHelperText>
+              <TextField 
+                fullWidth
+                id="standard-basic"
+                color="info"
+                type="url"
+                variant="standard"
+                {...register('website')}
+              />
+            </FormControl>
+
+            <FormControl sx={{ flex: 1 }}>
+              <FormHelperText sx={{fontFamily: 'Oswald', fontWeight: 500, margin: '10px 0', fontSize: 14, color: "#008E87" }}>Enter an email address</FormHelperText>
+              <TextField 
+                fullWidth
+                id="standard-basic"
+                color="info"
+                type="email"
+                variant="standard"
+                {...register('email')}
+              />
+            </FormControl>
+          </Stack>
 
           <Stack direction="column" gap={1} justifyContent="center" mb={2}>
             <Stack direction="row" gap={2}>
